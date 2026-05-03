9955cee378654afbaf909045da22f46245948fdc
kostaskoro99 <t8230062@aueb.gr>
2026-05-03 22:21:02 +0300

    Add import statements to rendezvous.py

--- rendezvous.py
+++ rendezvous.py
@@ -1,3 +1,5 @@
 # assignment-2026-2
-
 Second assignment for the 2026 course. The assignment can be found [here](https://github.com/dmst-algorithms-course/assignment-2026-2/blob/main/rendezvous.pdf).
+import sys
+import deque
+import bisect
