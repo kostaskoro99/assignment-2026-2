8146373b368fc314bca91116160c627326acec4d
kostaskoro99 <t8230062@aueb.gr>
2026-05-09 21:09:00 +0300

    Update rendezvous.py

--- rendezvous.py
+++ rendezvous.py
@@ -3,3 +3,9 @@ Second assignment for the 2026 course. The assignment can be found [here](https:
 import sys
 from collections import deque
 import bisect
+
+class GraphNavigator:
+    def __init__(self, num_nodes, edges, is_directed=False):
+        self.n = num_nodes
+        self.adj = [[] for _ in range(num_nodes)]
+        self.is_directed = is_directed
