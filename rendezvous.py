3bb34e3cad0a1b0c9baaa87643801e490b52d917
kostaskoro99 <t8230062@aueb.gr>
2026-05-13 13:40:49 +0300

    Update rendezvous.py

--- rendezvous.py
+++ rendezvous.py
@@ -1,5 +1,5 @@
 # assignment-2026-2
-Second assignment for the 2026 course. The assignment can be found [here](https://github.com/dmst-algorithms-course/assignment-2026-2/blob/main/rendezvous.pdf).
+
 import sys
 from collections import deque
 import bisect
@@ -16,9 +16,9 @@ class GraphNavigator:
                 bisect.insort(self.adj[v], u)
 
     def find_parity_meeting(self, start_a, start_b):
-        ""  Αναζήτηση σημείου συνάντησης (node, parity).
-        Επιστρέφει (node, time, path_a, path_b) ή None.
-        ""
+        # Αναζήτηση σημείου συνάντησης (node, parity).
+        # Επιστρέφει (node, time, path_a, path_b) ή None.
+        
         # bfs_data[node][parity] = dist 
         def get_bfs_map(start_node):
             dist_map = {}
